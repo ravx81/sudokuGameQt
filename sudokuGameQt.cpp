43fb04f1fdb183019916395afb33c466cb7dc2f8
nightravenx81 <nightravenx81@gmail.com>
2025-03-20 14:24:21 +0100

    Changed buttons place

--- sudokuGameQt.cpp
+++ sudokuGameQt.cpp
@@ -1,14 +1,20 @@
-#include "sudokuGameQt.h"
-#include "functions.h"
+#include <QApplication>
+#include <QMainWindow>
 #include <QTableWidget>
 #include <QHeaderView>
 #include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QGridLayout>
+#include <QPushButton>
 #include <QTableWidgetItem>
 #include <QStyledItemDelegate>
 #include <QLineEdit>
 #include <QRegularExpression>
 #include <QRegularExpressionValidator>
+#include <QWidget>
+#include <iostream>
 
+#include "functions.h" 
 
 class SingleCharDelegate : public QStyledItemDelegate
 {
@@ -22,63 +28,71 @@ public:
         const QStyleOptionViewItem& option,
         const QModelIndex& index) const override
     {
-        // Create a line edit with a single-character limit
         QLineEdit* editor = new QLineEdit(parent);
         editor->setMaxLength(1);
-
-        // Optionally, if you want to allow only digits 1-9, use a validator:
-        QRegularExpression re("[1-9]");  // digits 1 through 9
+        QRegularExpression re("[1-9]");
         QValidator* validator = new QRegularExpressionValidator(re, editor);
         editor->setValidator(validator);
-
         return editor;
     }
 };
 
 
+class sudokuGameQt : public QMainWindow
+{
+    Q_OBJECT
+public:
+    sudokuGameQt(QWidget* parent = nullptr);
+    ~sudokuGameQt();
+
+private slots:
+    void onDrawBoardClicked();
+    void onSolveBoardClicked();
+
+private:
+    QWidget* centralWidget;
+    QTableWidget* tableWidget;
+    QPushButton* button1;
+    QPushButton* button2;
+};
+
 sudokuGameQt::sudokuGameQt(QWidget* parent)
     : QMainWindow(parent)
 {
-    setFixedSize(900, 650);
-
+    setFixedSize(800, 600);
     setStyleSheet(
         "QMainWindow {"
         "    background-image: url(background.png);"
         "    background-repeat: no-repeat;"
-        "    background-position: top-center;"
+        "    background-position: top left;"
         "    background-size: cover;"
         "}"
     );
 
+    
     centralWidget = new QWidget(this);
     centralWidget->setAttribute(Qt::WA_TranslucentBackground);
     setCentralWidget(centralWidget);
 
-    layout = new QVBoxLayout(centralWidget);
-    layout->setContentsMargins(0, 0, 0, 0);
-    layout->setSpacing(0);
+   
+    QHBoxLayout* mainLayout = new QHBoxLayout(centralWidget);
+    mainLayout->setContentsMargins(10, 10, 10, 10);
+    mainLayout->setSpacing(10);
 
+    
     tableWidget = new QTableWidget(9, 9, this);
     tableWidget->setItemDelegate(new SingleCharDelegate(this));
-
-
-
     tableWidget->horizontalHeader()->setVisible(false);
     tableWidget->verticalHeader()->setVisible(false);
-
     tableWidget->horizontalHeader()->setSectionResizeMode(QHeaderView::Fixed);
     tableWidget->verticalHeader()->setSectionResizeMode(QHeaderView::Fixed);
 
-    int cellSize = 47;
+    int cellSize = 48;
     tableWidget->horizontalHeader()->setDefaultSectionSize(cellSize);
     tableWidget->verticalHeader()->setDefaultSectionSize(cellSize);
-
     tableWidget->setFrameStyle(QFrame::NoFrame);
-
     tableWidget->setShowGrid(false);
-
     tableWidget->viewport()->setAutoFillBackground(false);
-
     tableWidget->setStyleSheet(
         "QTableWidget {"
         "    background-color: rgba(255, 255, 255, 0);"
@@ -88,51 +102,42 @@ sudokuGameQt::sudokuGameQt(QWidget* parent)
         "}"
     );
 
+    
     for (int row = 0; row < 9; ++row) {
         for (int col = 0; col < 9; ++col) {
             QTableWidgetItem* item = new QTableWidgetItem("");
             item->setTextAlignment(Qt::AlignCenter);
             tableWidget->setItem(row, col, item);
-            
         }
     }
     int totalWidth = (cellSize * 9) + tableWidget->frameWidth() * 2;
     int totalHeight = (cellSize * 9) + tableWidget->frameWidth() * 2;
     tableWidget->setFixedSize(totalWidth, totalHeight);
 
-    QHBoxLayout* centerLayout = new QHBoxLayout;
-    centerLayout->setContentsMargins(0, 0, 0, 0);
-    centerLayout->addStretch();         
-    centerLayout->addWidget(tableWidget);
-    centerLayout->addStretch(); 
+    
+    mainLayout->addWidget(tableWidget, 0, Qt::AlignLeft | Qt::AlignTop);
 
    
-    layout->addLayout(centerLayout);
-
-   
-    button1 = new QPushButton("Draw board", this);
-    button2 = new QPushButton("Solve board", this);
+    QWidget* rightWidget = new QWidget(this);
+    QVBoxLayout* rightLayout = new QVBoxLayout(rightWidget);
+    rightLayout->setContentsMargins(0, 0, 0, 0);
+    rightLayout->setSpacing(10);
 
     
-    buttonLayout = new QHBoxLayout;
+    button1 = new QPushButton("Draw board", this);
+    button2 = new QPushButton("Solve board", this);
+    button1->setFixedSize(100, 60);
+    button2->setFixedSize(100, 60);
+    QHBoxLayout* buttonLayout = new QHBoxLayout;
     buttonLayout->addWidget(button1);
     buttonLayout->addWidget(button2);
+    rightLayout->addLayout(buttonLayout);
 
-   
-    layout->addLayout(buttonLayout);
-
-   
-    connect(button1, &QPushButton::clicked, this, &sudokuGameQt::onDrawBoardClicked);
-    connect(button2, &QPushButton::clicked, this, &sudokuGameQt::onSolveBoardClicked);
-
-
-
+    
     QGridLayout* keypadLayout = new QGridLayout;
-
     for (int i = 1; i < 10; i++) {
         QPushButton* button = new QPushButton(QString::number(i), this);
         button->setFixedSize(50, 50);
-
         connect(button, &QPushButton::clicked, [this, i]() {
             QTableWidgetItem* currentItem = tableWidget->currentItem();
             if (currentItem) {
@@ -141,31 +146,42 @@ sudokuGameQt::sudokuGameQt(QWidget* parent)
             });
         int row = (i - 1) / 3;
         int col = (i - 1) % 3;
-        keypadLayout->addWidget(button, row, col);       
+        keypadLayout->addWidget(button, row, col);
     }
- 
+
+    
     QPushButton* clearButton = new QPushButton("Clear", this);
+    clearButton->setFixedSize(100, 60);
     connect(clearButton, &QPushButton::clicked, [this]() {
         QTableWidgetItem* currentItem = tableWidget->currentItem();
         if (currentItem) {
             currentItem->setText("");
         }
-    });
-    keypadLayout->addWidget(clearButton, 3, 0, 1, 3);
+        });
+    keypadLayout->addWidget(clearButton, 3, 0, 1, 3, Qt::AlignCenter);
 
-    layout->addLayout(keypadLayout);
+    rightLayout->addLayout(keypadLayout);
 
+    
+    rightLayout->addStretch();
+
+    
+    mainLayout->addWidget(rightWidget, 0, Qt::AlignTop | Qt::AlignRight);
+
+    
+    connect(button1, &QPushButton::clicked, this, &sudokuGameQt::onDrawBoardClicked);
+    connect(button2, &QPushButton::clicked, this, &sudokuGameQt::onSolveBoardClicked);
 }
 
 sudokuGameQt::~sudokuGameQt()
 {
-    
 }
 
 void sudokuGameQt::onDrawBoardClicked()
 {
     int board[9][9] = { 0 };
 
+    
     fillBoard(board);
     prepareBoard(board, 40);
 
@@ -180,7 +196,6 @@ void sudokuGameQt::onDrawBoardClicked()
         }
     }
 }
-
 
 void sudokuGameQt::onSolveBoardClicked()
 {
@@ -207,5 +222,5 @@ void sudokuGameQt::onSolveBoardClicked()
     else {
         std::cout << "Problems";
     }
-    
 }
+
